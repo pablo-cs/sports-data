4e6237c7a255d257cbb83087a68d48779f1bb436
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-07-12 17:34:41 +0000

    Accounted for player not found error

--- app/access_api.py
+++ app/access_api.py
@@ -2,20 +2,18 @@ import requests
 
 def get_player_data(player_name):
     player_url = f"https://www.balldontlie.io/api/v1/players?search={player_name}"
-    response = requests.get(player_url)
+    response = requests.get(player_url).json()
    
-    if response.status_code == 200:
-        player_data = response.json()['data'][0]
-        player_data['name'] = player_data['first_name'] + " " + player_data['last_name']
-        player_id = player_data['id']
-        stats_url = f"https://www.balldontlie.io/api/v1/season_averages?player_ids[]={player_id}"
-        response = requests.get(stats_url).json()
-        if len(response['data']) > 0:
-            stats_data = response['data'][0]
-            player_data.update(stats_data)
-        return player_data
-    else:
-        print("Error: Failed to retrieve player data.")
+    if len(response['data']) == 0:
         return None
+    player_data = response['data'][0]
+    player_data['name'] = player_data['first_name'] + " " + player_data['last_name']
+    player_id = player_data['id']
+    stats_url = f"https://www.balldontlie.io/api/v1/season_averages?player_ids[]={player_id}"
+    response = requests.get(stats_url).json()
+    if len(response['data']) > 0:
+        stats_data = response['data'][0]
+        player_data.update(stats_data)
+    return player_data
 
 
